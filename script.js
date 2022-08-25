ca1e5ecbe3f2f18aa19b5bf92a24d3b34a85921f
ian <newbieian6@gmail.com>
2022-08-25 13:49:14 -0300

    Add reset button
    Make a reset button for the user to click and replace the current grid with a new one with a new desired grid size

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 const container = document.getElementById("container");
+const button = document.getElementById("resetButton");
 
-function makeGrid(rows, cols) {
+let userInput = 0;
+let rows = userInput;
+let cols = userInput;
+
+function makeGrid() {  
+    // Get user input
+    while (1>0) {
+        userInput = prompt("Please enter your grid size. Maximum: 100");
+        if (userInput > 0 && userInput < 101) {
+            break;
+        };
+    };
+    rows = userInput;
+    cols = userInput;
+
+    // Make the grid skeleton
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
+    // Loop to create and append grid items to the div container
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
         cell.id = `grid-item${i+1}`;
     };
+
+    // Add event listener to change grid cells color when you hover them with the mouse
+    for ((i = 0); i < (rows * cols); i++) {
+        let item = document.getElementById(`grid-item${i+1}`);
+        item.addEventListener('mouseenter', changeClass);
+    };
 };
 
-
-let userInput = prompt("Please enter your grid size")
-const rows = userInput;
-const cols = userInput;
-makeGrid(rows, cols);
-
+// Make function to add the hovered css class to the event target
 function changeClass(e) {
     e.target.classList.add("hovered");
 };
 
-for ((i = 0); i < (rows * cols); i++) {
-    let item = document.getElementById(`grid-item${i+1}`);
-    item.addEventListener('mouseenter', changeClass);
+// Create function to remove the current sketch and then call makeGrid to make a new one
+function cleanGrid() {
+    for ((i = 0); i < (rows * cols); i++) {
+        let item = document.getElementById(`grid-item${i+1}`);
+        item.remove();
+    };
+    makeGrid();
 };
+
+// Add event listener to call cleanGrid when the reset button is clicked
+button.addEventListener('click', cleanGrid);
+
+makeGrid();
